bbcceb6d9ededc5ef29cf63280be35135f365adc
Mina G <mghaffar89@gmail.com>
2020-06-23 22:23:35 -0500

    added inquirer questions and inquirer prompt to initialize app

--- Develop/app.js
+++ Develop/app.js
@@ -10,7 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -33,3 +32,96 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const employeeList = [];
+
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Please enter employee name:",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Please enter employee ID number:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter employee e-mail address:",
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is the employee's role in the team?",
+    choices: [
+      {
+        name: "Manager",
+      },
+      {
+        name: "Engineer",
+      },
+      {
+        name: "Intern",
+      },
+    ],
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Please enter manager's office number:",
+    when: function (answers) {
+      return answers.role === "Manager";
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Please enter engineer's github username:",
+    when: function (answers) {
+      return answers.role === "Engineer";
+    },
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is the name of the school you're currently attending?",
+    when: function (answers) {
+      return answers.role === "Intern";
+    },
+  },
+];
+
+function promptUser() {
+  inquirer.prompt(questions).then(function (response) {
+    if (response.role === "Manager") {
+      const newManager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber
+      );
+      employeeList.push(newManager);
+    } else if (response.role === "Engineer") {
+      const newEngineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      employeeList.push(newEngineer);
+    } else if (response.role === "Intern") {
+      const newIntern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      employeeList.push(newIntern);
+    }
+    newEmployee();
+  });
+}
+
+promptUser();
