6da07fdd81d946907122ce37fa002cc47cbcae68
Mina G <mghaffar89@gmail.com>
2020-06-23 22:50:02 -0500

    added classes for engineer and manager

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,2 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 //Reference Activity 10.20 & 21
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+}
+
+module.exports = Engineer;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,2 +1,16 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 //Reference Activity 10.20 & 21
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+}
+
+module.exports = Manager;
